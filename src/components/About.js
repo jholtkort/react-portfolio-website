479c2b9f56593fa7bcf44a94535a7b7590cc4340
Josh Holtkort <jholtkort@utexas.edu>
2019-05-01 16:35:48 -0500

    Updated about and home sections

--- src/components/About.js
+++ src/components/About.js
@@ -4,37 +4,42 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div className="about-text container">
-      <h1>About Me:</h1>
-      <h5 className="mt-3">
-        I was raised in San Angelo, TX, and have a Bachelor of Science in
-        Mechanical Engineering and a Business Foundations Certificate from The
-        University of Texas at Austin.
-      </h5>
-      <h5 className="mt-3">
-        While working as a Cost Estimator for two and a half years at AECOM, I
-        found an interest in software development and decided that I wanted to
-        pursue a career as a software developer. Since that time, I have earned
-        the Responsive Web Design Certification, JavaScript Algorithms and Data
-        Structures Certification, and Front End Libraries Certification from
-        freeCodeCamp; completed the Modern React with Redux, Python and Flask
-        Bootcamp, and Git Started with GitHub courses on Udemy.
-      </h5>
-      <h5 className="mt-3">
-        This has given me experience using React, Redux, JavaScript, HTML5,
-        CSS3, Bootstrap 3 and 4, and SASS. I encourage you to visit the{" "}
-        <a href="/projects">Projects</a> tab, as well as, my{" "}
-        <a href="https://github.com/jholtkort">GitHub</a> profile where I have
-        been pushing my projects and activities to.
-      </h5>
-      <h5 className="mt-3">
-        In my spare time I enjoy swimming, biking, running, and spending time
-        outdoors. Coming from a swimming background in high school, I quickly
-        found a love for the sport of triathlon, and competed for the Texas
-        Triathlon team while I was at UT Austin. I have since gone on to
-        complete many triathlons and endurance events, including 3 half
-        Ironmans, 1 full Ironman, and 2 marathons.
-      </h5>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-10 col-lg-10 about-text">
+          <h1>About Me:</h1>
+          <h5 className="mt-3">
+            I was raised in San Angelo, TX, and have a Bachelor of Science in
+            Mechanical Engineering and a Business Foundations Certificate from
+            The University of Texas at Austin.
+          </h5>
+          <h5 className="mt-3">
+            While working as a Cost Estimator for two and a half years at AECOM,
+            I found an interest in software development and decided that I
+            wanted to pursue a career as a software developer. Since that time,
+            I have earned the Responsive Web Design Certification, JavaScript
+            Algorithms and Data Structures Certification, and Front End
+            Libraries Certification from freeCodeCamp; completed the Modern
+            React with Redux, Python and Flask Bootcamp, and Git Started with
+            GitHub courses on Udemy.
+          </h5>
+          <h5 className="mt-3">
+            This has given me experience using React, Redux, JavaScript, HTML5,
+            CSS3, Bootstrap 3 and 4, and SASS. I encourage you to visit the{" "}
+            <a href="/projects">Projects</a> tab, as well as, my{" "}
+            <a href="https://github.com/jholtkort">GitHub</a> profile where I
+            have been pushing my projects and activities to.
+          </h5>
+          <h5 className="mt-3">
+            In my spare time I enjoy swimming, biking, running, and spending
+            time outdoors. Coming from a swimming background in high school, I
+            quickly found a love for the sport of triathlon, and competed for
+            the Texas Triathlon team while I was at UT Austin. I have since gone
+            on to complete many triathlons and endurance events, including 3
+            half Ironmans, 1 full Ironman, and 2 marathons.
+          </h5>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/HomeText.js
+++ src/components/HomeText.js
@@ -4,20 +4,24 @@ import "./HomeText.css";
 
 const HomeText = () => {
   return (
-    <div className="home-text container">
-      <h1>Welcome to the website of Josh Holtkort</h1>
-      <h5 className="mt-3">
-        I am a software developer based out of Austin, TX, and am passionate
-        about making responsive software and web applications that solve
-        client's biggest challenges.
-      </h5>
-      <div className="icons">
-        <a className="mr-5" href="https://github.com/jholtkort">
-          <i className="fab fa-github fa-3x" />
-        </a>
-        <a href="https://www.linkedin.com/in/joshholtkort">
-          <i className="fab fa-linkedin-in fa-3x" />
-        </a>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-10 home-text">
+          <h1>Welcome to the website of Josh Holtkort</h1>
+          <h5 className="my-3">
+            I am a software developer based out of Austin, TX, and am passionate
+            about making responsive software and web applications that solve
+            clients' biggest challenges.
+          </h5>
+          <div className="icons">
+            <a className="mr-5" href="https://github.com/jholtkort">
+              <i className="fab fa-github fa-3x" />
+            </a>
+            <a href="https://www.linkedin.com/in/joshholtkort">
+              <i className="fab fa-linkedin-in fa-3x" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
