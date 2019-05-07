b16cccf38a2cef9ce319eb32cf6dd96ef84e7825
Josh Holtkort <jholtkort@utexas.edu>
2019-05-07 14:27:42 -0500

    Updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { projects } from "../projectList/projects";
 import "./Projects.css";
 
 const Projects = () => {
@@ -12,7 +13,30 @@ const Projects = () => {
             <hr />
 
             <div className="row justify-content-around">
-              <div className="card col-10 col-lg-5 my-5">
+              {projects.map(x => (
+                <div className="card col-10 col-lg-5 my-5" key={x.id}>
+                  <img
+                    src={x.image}
+                    alt={x.title}
+                    className="card-img-top my-3"
+                  />
+                  <h5>{x.title}</h5>
+                  <p className="card-text">{x.description}</p>
+                  <div className="row justify-content-around mb-3">
+                    <a
+                      className="btn btn-primary btn-lg my-3"
+                      href={x.websiteLink}
+                    >
+                      {x.title} Homepage
+                    </a>
+                    <a className="mr-5" href={x.gitHubLink}>
+                      <i className="fab fa-github fa-3x my-3 ml-3" />
+                    </a>
+                  </div>
+                </div>
+              ))}
+
+              {/* <div className="card col-10 col-lg-5 my-5">
                 <img
                   src="https://images.unsplash.com/photo-1547845662-08919068676f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1368&q=80"
                   alt="weather app"
@@ -66,7 +90,7 @@ const Projects = () => {
                     <i className="fab fa-github fa-3x" />
                   </a>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
---
+++ src/projectList/projects.js
@@ -0,0 +1,45 @@
+// array of projects
+export const projects = [
+  {
+    id: "1",
+    image:
+      "https://images.unsplash.com/photo-1547845662-08919068676f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1368&q=80",
+    title: "Weather App",
+    description:
+      "React application that fetches weather information from the Open Weather Map API, and renders a background based off of the received data.",
+    websiteLink: "https://jholtkort.github.io/weather-app/",
+    gitHubLink: "https://github.com/jholtkort/weather-app"
+  },
+
+  {
+    id: "2",
+    image:
+      "https://images.unsplash.com/photo-1519892300165-cb5542fb47c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    title: "Drum Machine",
+    description:
+      "React application that plays a different audio file for each key press that corresponds to each button.",
+    websiteLink: "https://jholtkort.github.io/drum-machine/",
+    gitHubLink: "https://github.com/jholtkort/drum-machine"
+  },
+
+  {
+    id: "3",
+    image:
+      "https://images.unsplash.com/photo-1548360247-d7f05b9b4865?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    title: "Reactive Webpage",
+    description:
+      "React application that uses Bootstrap 4 to create a responsive webpage layout.",
+    websiteLink: "https://jholtkort.github.io/responsive-webpage/",
+    gitHubLink: "https://github.com/jholtkort/responsive-webpage"
+  }
+
+  // Blank object for future projects
+  // {
+  //     id:
+  //     image:
+  //     title:
+  //     description:
+  //     websiteLink:
+  //     gitHubLink:
+  // }
+];
