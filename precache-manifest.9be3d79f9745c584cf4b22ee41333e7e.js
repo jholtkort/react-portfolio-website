2dd66bea41411ae96e50094ff2546eaf0d008a41
Josh Holtkort <jholtkort@utexas.edu>
2019-10-14 15:24:21 -0500

    Updates

---
+++ precache-manifest.9be3d79f9745c584cf4b22ee41333e7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf215bb1554ce392d3f36ba93cfac60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f47dc288d995c27d369",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a0d308d15fc52cc97d4f",
+    "url": "/static/css/main.d6ee3abf.chunk.css"
+  },
+  {
+    "revision": "3f47dc288d995c27d369",
+    "url": "/static/js/2.6c7a00cb.chunk.js"
+  },
+  {
+    "revision": "a0d308d15fc52cc97d4f",
+    "url": "/static/js/main.901dd528.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89e9f7e26197fe730226428f141e08f8",
+    "url": "/static/media/josh-holtkort-resume.89e9f7e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6fb24ce8135a564708adf7dafb91c2a3.js"
+  "/precache-manifest.9be3d79f9745c584cf4b22ee41333e7e.js"
 );
 
 self.addEventListener('message', (event) => {
